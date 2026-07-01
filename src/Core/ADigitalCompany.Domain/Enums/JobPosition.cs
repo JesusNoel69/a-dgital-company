43872b4c7ece6c8d38c74201e8742cf5ccaedd78
Jesus <jnoel883@gmail.com>
2026-07-01 14:00:02 -0600

    Add initial job positions

--- src/Core/ADigitalCompany.Domain/Enums/JobPosition.cs
+++ src/Core/ADigitalCompany.Domain/Enums/JobPosition.cs
@@ -2,6 +2,23 @@ namespace ADigitalCompany.Domain.Enums
 {
     public enum JobPosition
     {
-        
+        Operator,
+        Technician,
+        MaintenanceTechnician,
+        QualityInspector,
+        ProductionEngineer,
+        ProcessEngineer,
+        ManufacturingEngineer,
+        SoftwareDeveloper,
+        SystemAdministrator,
+        BusinessAnalyst,
+        Accountant,
+        Buyer,
+        HumanResources,
+        Supervisor,
+        Coordinator,
+        Manager,
+        Director,
+        Intern
     }
 }
