7761e5d09e5548c9cae6e1bcefc53e36a41aa11d
Jesus <jnoel883@gmail.com>
2026-07-07 19:42:11 -0600

    Add fault controllers for departments

--- src/ADigitalCompany.API/Controllers/DepartmentsController.cs
+++ src/ADigitalCompany.API/Controllers/DepartmentsController.cs
@@ -1,4 +1,10 @@
+using ADigitalCompany.Application.Features.Departments.Commands.CreateDepartment;
+using ADigitalCompany.Application.Features.Departments.Commands.DeleteDepartment;
+using ADigitalCompany.Application.Features.Departments.Commands.UpdateDepartment;
+using ADigitalCompany.Application.Features.Departments.Queries.GetDepartmentById;
+using ADigitalCompany.Application.Features.Departments.Queries.GetDepartments;
 using ADigitalCompany.Application.Features.Employees.Queries.GetEmployeesByDepartment;
+using ADigitalCompany.Application.Models.Department;
 using ADigitalCompany.Application.Models.Employee;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -10,23 +16,50 @@ namespace ADigitalCompany.API.Controllers
     public class DepartmentsController(IMediator mediator) : ControllerBase
     {
         private readonly IMediator _mediator = mediator;
-        /*
-        GET    /api/departments
 
-        GET    /api/departments/{id}
-
-        GET    /api/departments/{id}/employees
-
-        POST   /api/departments
-
-        PUT    /api/departments/{id}
-
-        DELETE /api/departments/{id}
-        */
         [HttpGet("{id:int}/employees")]
         public async Task<ActionResult<IReadOnlyList<EmployeeDto>>> GetEmployees(int id)
         {
             return Ok(await _mediator.Send(new GetEmployeesByDepartmentQuery(id)));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<DepartmentDto>>> GetAll()
+        {
+            return Ok(await _mediator.Send(new GetDepartmentsQuery()));
+        }
+
+        [HttpGet("{id:int}/departments")]
+        public async Task<ActionResult<DepartmentDto>> GetById(int id)
+        {
+            return Ok(await _mediator.Send(new GetDepartmentByIdQuery(id)));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create([FromBody] CreateDepartmentCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetById), new { id }, id);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateDepartmentCommand command)
+        {
+            if (id != command.Id)
+                return BadRequest();
+
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteDepartmentCommand { Id = id });
+
+            return NoContent();
         }
     }
 }
