10d19055f50dd088b56fd9844754644b334e1b22
Jesus <jnoel883@gmail.com>
2026-07-27 13:22:48 -0600

    Add register route

--- src/UI/WebUI/src/app/app.routes.ts
+++ src/UI/WebUI/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PageLayout } from './layout/page-layout/page-layout';
 import { Badge } from './shared/components/badge/badge';
 import { Login } from './features/auth/pages/login/login';
+import { Register } from './features/auth/pages/register/register';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,7 @@ export const routes: Routes = [
     children: [{ path: 'dashboard/badge', component: Badge }],
   },
   { path: 'badge', component: Badge },
+  { path: 'register', component: Register },
   { path: '', component: PageLayout },
   { path: 'login', component: Login },
   //  { path: '**', component: NotFoundComponent }, //
