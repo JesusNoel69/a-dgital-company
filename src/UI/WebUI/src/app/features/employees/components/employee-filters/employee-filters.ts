a92353fbecc01586fbd85aee1d2b4b4ef38c82ce
Jesus <jnoel883@gmail.com>
2026-08-12 09:58:51 -0600

    Fix filters UI

--- src/UI/WebUI/src/app/features/employees/components/employee-filters/employee-filters.ts
+++ src/UI/WebUI/src/app/features/employees/components/employee-filters/employee-filters.ts
@@ -11,8 +11,13 @@ import { Employee } from '../../../../core/models/Employee';
 export class EmployeeFilters {
   sortField: keyof Employee | null = null;
   sortDirection: 'asc' | 'desc' = 'asc';
+  query: string = '';
 
   constructor(private employeeService: EmployeeService) {}
+
+  search() {
+    console.log(this.query);
+  }
 
   changeSort(field: keyof Employee): void {
     if (this.sortField === field) {
